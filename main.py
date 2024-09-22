857d3c62972bf7782ecb676e77c5f2e440f88cb2
Nikola4667636 <151454196+Nikola4667636@users.noreply.github.com>
2024-09-22 16:44:07 +0300

    Вывод произведения в main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 print("Здарвствуйте! Введите 2 целых числа")
 a, b = int(input()), int(input())
-<<<<<<< HEAD
 print("Сумма - ", a + b)
-=======
 print("Разность -", a - b)
->>>>>>> f7fcc187dbdb43395dcdbb1bcb5080d9ff9eca87
+print("Произведение - ", a * b)
